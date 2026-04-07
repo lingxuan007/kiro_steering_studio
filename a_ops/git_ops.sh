0f7bf6dbb0c256d9fb81d4e0cd50a7619abd887b
LingXuan <251381922@qq.com>
2026-04-07 20:33:50 +0800

    mac_pro_tq_git

--- a_ops/git_ops.sh
+++ a_ops/git_ops.sh
@@ -1,22 +1,39 @@
-### Windows 微星主机
+# ----------- Windows 微星主机 --------------------
 
-# cd C:\Users\Murra\Desktop\code\kiro\kiro_steering_studio
-git pull origin main
-# git init    # 开始初始化
-# git remote add origin git@github.com:lingxuan007/kiro_steering_studio.git    # 添加远程仓库
+    # cd C:\Users\Murra\Desktop\code\kiro\kiro_steering_studio
+    # git pull origin main
 
-# git branch  # 查看分支
-# git branch -m windows_git        # 重命名分支
+    # git branch
+    # git branch -m windows_git        # 重命名分支
 
-
-
-
-cd C:\Users\Murra\Desktop\code\kiro\kiro_steering_studio
-git add --all .                          # 将文件添加到暂存区
-git commit -m "windows-微星"             # 提交更改
-git push -u origin windows_git:main      # 推送
+    # cd C:\Users\Murra\Desktop\code\kiro\kiro_steering_studio
+    # git add --all .      
+    # git commit -m "windows-微星"
+    # git push -u origin windows_git:main
 
 
+    # git init
+    # git remote add origin git@github.com:lingxuan007/kiro_steering_studio.git
+# --------------------------------------------
 
-##################
+
+
+# ----------- Mac Pro --------------------
+
+    # cd /Users/huanglingxuan/Documents/code/kiro/kiro_steering_studio
+    # git pull origin main
+
+
+    # git branch
+    # git branch -m mac_pro_tq_git        # 重命名分支
+
+cd /Users/huanglingxuan/Documents/code/kiro/kiro_steering_studio
+git add --all .
+git commit -m "mac_pro_tq_git"
+git push -u origin mac_pro_tq_git:main
+
+
+    # git init
+    # git remote add origin git@github.com:lingxuan007/kiro_steering_studio.git
+# --------------------------------------------
 
